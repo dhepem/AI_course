67978592f4beaddba373bcf14a21ad9a78beae46
Hamin <hamin2633@naver.com>
2022-10-19 13:17:23 +0900

    Django study

--- Django/webproj/homepage/models.py
+++ Django/webproj/homepage/models.py
@@ -1,3 +1,20 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+
+class Coffee(models.Model):
+    def __str__(self):
+        return self.name
+    name = models.CharField(default="", max_length=30)
+    price = models.IntegerField(default=0)
+    is_ice = models.BooleanField(default=False)
+
+    """
+    문자열 : CharField
+    숫자 : IntegerField, SmallIntegerField, ...
+    논리형 : BooleanField
+    시간/날짜 : DateTimeField
+    ...
+    """
--- Django/webproj/homepage/views.py
+++ Django/webproj/homepage/views.py
@@ -1,5 +1,6 @@
-from http.client import HTTPResponse
 from django.shortcuts import HttpResponse, render
+from .models import Coffee
+from .forms import CoffeeForm
 
 # Create your views here.
 
@@ -7,5 +8,17 @@ from django.shortcuts import HttpResponse, render
 def index(request):
     # return HttpResponse("<h1>Hello World!</h1>")
     nums = [1, 2, 3, 4, 5]
-
     return render(request, 'index.html', {"my_list": nums})
+
+
+def coffee_view(request):
+    coffee_all = Coffee.objects.all()  # .get(), .filter(), ...
+    # 만약 request가 POST라면:
+    # POST를 바탕으로 Form을 완성하고
+    # Form이 유효하면 -> 저장!
+    if request.method == "POST":
+        form = CoffeeForm(request.POST)  # 완성된 Form
+        if form.is_valid():  # 채워진 Form이 유효하다면:
+            form.save()  # 이 Form 내용을 Model에 저장
+    form = CoffeeForm()
+    return render(request, 'coffee.html', {"coffee_list": coffee_all, "coffee_form": form})
